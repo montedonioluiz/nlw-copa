f48a9a5a42f8827e41863336a065f9588e58ae9e
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2022-11-02 11:48:17 -0300

    mobile svg config && signin page

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,23 @@
-import { Center, Text } from "native-base";
+import { Center, Icon, Text } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.600">
-      <Text color="white" fontSize="24" fontFamily="heading">
-        SignIn
+    <Center flex={1} bgColor="gray.600" p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title="ENTRAR COM GOOGLE"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+        de sua conta.
       </Text>
     </Center>
   );
